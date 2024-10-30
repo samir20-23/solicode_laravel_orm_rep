8ad5da579fef1609c6e1c4be3b4bb613d63ce5a2
samir20-23 <germanysamir1@gmail.com>
2024-10-30 08:19:15 +0000

    tuto_1

--- TUTORIALS/TUTOR_1/routes/web.php
+++ TUTORIALS/TUTOR_1/routes/web.php
@@ -3,6 +3,11 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return 'Hello, Laravel!';
 });
+
+Route::get('/about', function () {
+    return view('about');
+});
+
 // test test
